e60fb8b1251ce9e068dfcb5c9727e110db681159
Ghris01 <chris.agg2001@gmail.com>
2023-08-06 22:49:16 -0700

    comite login 01

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,14 +44,14 @@
                                             </div>
                                         @endif
                                             <div class="form-group mb-20">
-                                                <label for="email">Username Or Email Address</label>
+                                                <label for="email">Correo electronico</label>
                                                 <input type="text" class="form-control" id="email" name="email" value="admin@gmail.com" placeholder="Email address">
                                                 @if($errors->has('email'))
                                                     <p class="text-danger">{{$errors->first('email')}}</p>
                                                 @endif
                                             </div>
                                             <div class="form-group mb-15">
-                                                <label for="password-field">password</label>
+                                                <label for="password-field">Contraseña</label>
                                                 <div class="position-relative">
                                                     <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" value="admin">
                                                     <span toggle="#password-field" class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2"></span>
@@ -64,53 +64,24 @@
                                                 <div class="checkbox-theme-default custom-checkbox ">
                                                     <input class="checkbox" type="checkbox" id="check-1">
                                                     <label for="check-1">
-                                                        <span class="checkbox-text">Keep me logged in</span>
+                                                        <span class="checkbox-text">Recordarmé</span>
                                                     </label>
                                                 </div>
-                                                <a href="{{ route('forget_password') }}">forget password?</a>
+                                                <a href="{{ route('forget_password') }}">¿Olvidaste tu contraseña?</a>
                                             </div>
                                             <div class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
                                                 <button class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
-                                                    sign in
+                                                    Iniciar sesión
                                                 </button>
                                             </div>
                                         </div>
                                     </form>
                                 </div>
-                                <div class="px-20">
-                                    <p class="social-connector social-connector__admin text-center">
-                                        <span>Or</span>
-                                    </p>
-                                    <div class="button-group d-flex align-items-center justify-content-center">
-                                        <ul class="admin-socialBtn">
-                                            <li>
-                                                <button class="btn text-dark google">
-                                                    <img class="svg" src="{{ asset('assets/img/google-Icon.svg') }}" alt="img" />
-                                                </button>
-                                            </li>
-                                            <li>
-                                                <button class=" radius-md wh-48 content-center facebook">
-                                                    <i class="uil uil-facebook-f"></i>
-                                                </button>
-                                            </li>
-                                            <li>
-                                                <button class="radius-md wh-48 content-center twitter">
-                                                    <i class="uil uil-twitter"></i>
-                                                </button>
-                                            </li>
-                                            <li>
-                                                <button class="radius-md wh-48 content-center github">
-                                                    <i class="uil uil-github"></i>
-                                                </button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
                                 <div class="admin-topbar">
                                     <p class="mb-0">
-                                        Don't have an account?
+                                    ¿No tienes una cuenta?
                                         <a href="{{ route('register') }}" class="color-primary">
-                                            Sign up
+                                            Crear una
                                         </a>
                                     </p>
                                 </div>
