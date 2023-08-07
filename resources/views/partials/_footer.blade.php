aa12aa0166b8325296ec30e208edaf52c15e6ca5
Ghris01 <chris.agg2001@gmail.com>
2023-08-07 00:50:45 -0700

    barra abajo borrar

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,16 +2,7 @@
     <div class="row">
         <div class="col-md-6">
             <div class="footer-copyright">
-                <p>© @php date_default_timezone_set('Asia/Dhaka') @endphp {{ date('Y') }}<a href="#">Sovware</a></p>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="footer-menu text-end">
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Team</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+                <p>© derechos reservados<a href="#">Localshop</a></p>
             </div>
         </div>
     </div>
