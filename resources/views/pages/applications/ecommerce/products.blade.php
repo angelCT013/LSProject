6e57a514f458f00769249d82c03c124ca394793b
Ghris01 <chris.agg2001@gmail.com>
2023-08-08 23:02:27 -0700

    a

--- resources/views/pages/applications/ecommerce/products.blade.php
+++ resources/views/pages/applications/ecommerce/products.blade.php
@@ -2,6 +2,8 @@
 @section('description',$description)
 @extends('layout.app')
 @section('content')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <div class="container-fluid">
     <div class="row">
@@ -538,7 +540,31 @@
             </div>
         </div>
 
+        <div class="modal fade" id="ModalEnlazar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modaltitle">Enlazar empresas</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-dismiss="modal" onclick="Guardar()">Guardar</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="closeModal()">Cerrar</button>
+
+    </div>
+    </div>
+  </div>
+</div>
+
         <script type='text/javascript'>
+        var modalEnlazar = document.getElementById('ModalEnlazar');
 
 
          $(document).ready(function(){
@@ -547,15 +573,12 @@
 
          function productos(){
             var tbody = document.querySelector('.tbody');
-            console.log(tbody);
-            var datos = {'tipo':'productos'};
            $.ajax({
             type:"GET",
             url:"/obtener-productos",
             datatype:'json',
             success: function(response){
                 let res = response;
-                console.log(response);
                 res.forEach(function(valor,index,array){
                     var producto_id = valor.producto_id;
                     var nombre = valor.nombre;
@@ -592,17 +615,12 @@
                                     +'<td>'
                                         +'<ul class="orderDatatable_actions mb-0 d-flex flex-wrap">'
                                             +'<li>'
-                                                +'<a href="#" class="view">'
-                                                    +'<i class="uil uil-setting"></i>'
-                                                +'</a>'
-                                            +'</li>'
-                                            +'<li>'
                                                 +'<a href="#" class="edit">'
-                                                    +'<i class="uil uil-edit"></i>'
+                                                    +'<i class="uil uil-edit" onclick="editar('+producto_id+')"></i>'
                                                 +'</a>'
                                             +'</li>'
                                             +'<li>'
-                                                +'<a href="#" class="remove">'
+                                                +'<a href="#" class="remove" onclick="borrar('+producto_id+')">'
                                                     +'<i class="uil uil-trash-alt"></i>'
                                                 +'</a>'
                                             +'</li>'
@@ -620,6 +638,23 @@
 
 
          }
+
+         function editar(data){
+                console.log('data '+data);
+                $(modalEnlazar).modal('show');
+
+         }
+
+         function borrar(id){
+                console.log('id '+id);
+         }
+
+         function closeModal(){
+        var ModalEnlazar = document.getElementById('ModalEnlazar');
+        $(ModalEnlazar).modal('hide');
+        }
+
+
         </script>
 
 
