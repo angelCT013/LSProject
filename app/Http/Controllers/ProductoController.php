b589c45bf149fea07d376c6224f5be4f8a924b0b
ZoelGtz <zoelgtz98@gmail.com>
2023-08-09 18:45:13 -0700

    si

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -31,28 +31,28 @@ class ProductoController extends Controller
         // return redirect()->back()->with('success', 'Producto agregado exitosamente.');
     }
     public function updateProducto(Request $request)
-{
-    // dd($request->all());
-    // Obtener los datos del formulario
-    $producto_id = $request->input('idproducto');
-    $nombre = $request->input('nombre');
-    $numSerie = $request->input('numSerie');
-    $status_id = $request->input('status_id');
-    $modelo_id = $request->input('modelo_id');
-    $categoria_id = $request->input('categoria_id');
-    // $imagen = $request->input('imagen');
-    $imagen = 'producto.png';
-    $descripcion = $request->input('descripcion');
+    {
+        // dd($request->all());
+        // Obtener los datos del formulario
+        $producto_id = $request->input('idproducto');
+        $nombre = $request->input('nombre');
+        $numSerie = $request->input('numSerie');
+        $status_id = $request->input('status_id');
+        $modelo_id = $request->input('modelo_id');
+        $categoria_id = $request->input('categoria_id');
+        // $imagen = $request->input('imagen');
+        $imagen = 'producto.png';
+        $descripcion = $request->input('descripcion');
 
-    // Llamar al procedimiento almacenado para actualizar el producto
-    \DB::statement("CALL actualizar_producto(?, ?, ?, ?, ?, ?, ?, ?)",
-        [$producto_id, $nombre, $numSerie, $status_id, $modelo_id, $categoria_id, $imagen, $descripcion]
-    );
+        // Llamar al procedimiento almacenado para actualizar el producto
+        \DB::statement("CALL actualizar_producto(?, ?, ?, ?, ?, ?, ?, ?)",
+            [$producto_id, $nombre, $numSerie, $status_id, $modelo_id, $categoria_id, $imagen, $descripcion]
+        );
 
-    // Redirigir a la vista con un mensaje de éxito
-    // return redirect()->route('ecommerce.products', ['locale' => app()->getLocale(), 'id' => $producto_id])->with('success', 'Producto actualizado exitosamente.');
-    return redirect()->route('ecommerce.products',app()->getLocale())->with('success', 'Producto actualizado exitosamente.');
+        // Redirigir a la vista con un mensaje de éxito
+        // return redirect()->route('ecommerce.products', ['locale' => app()->getLocale(), 'id' => $producto_id])->with('success', 'Producto actualizado exitosamente.');
+        return redirect()->route('ecommerce.products',app()->getLocale())->with('success', 'Producto actualizado exitosamente.');
 
-}
+    }
 
 }
