dbd15e43fcfc848d42914ecf680794b0bee031f9
legna013 <angel.developer013@gmail.com>
2023-08-10 20:50:27 -0700

    backup

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class BackupController extends Controller
 {
@@ -13,8 +13,11 @@ class BackupController extends Controller
             Artisan::call('backup:run', [
                 '--only-db' => true,
                 '--disable-notifications' => true,
-                '--filename' => $backupName, // Usar el nombre proporcionado en la solicitud
+                '--filename' => $backupName,
             ]);
+
+            // Llama al stored procedure para insertar el nombre en la tabla backups
+            DB::select('CALL sp_insert_Backuptable(?)', [$backupName]);
 
             return response()->json(['message' => 'Backup realizado exitosamente']);
         } catch (\Exception $e) {
