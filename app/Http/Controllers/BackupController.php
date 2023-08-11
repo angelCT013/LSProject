a095151c2a269448f1128fd9f92ed564efeaeea1
legna013 <angel.developer013@gmail.com>
2023-08-10 20:56:54 -0700

    controller

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 
 class BackupController extends Controller
 {
@@ -13,12 +13,12 @@ class BackupController extends Controller
             Artisan::call('backup:run', [
                 '--only-db' => true,
                 '--disable-notifications' => true,
-                '--filename' => $backupName,
+                '--filename' => $backupName, // Usar el nombre proporcionado en la solicitud
             ]);
-
             // Llama al stored procedure para insertar el nombre en la tabla backups
-            DB::select('CALL sp_insert_Backuptable(?)', [$backupName]);
-
+            // DB::select('CALL sp_insert_Backuptable(?)', [$backupName]);
+            \DB::statement("CALL sp_insert_Backuptable(?)",
+            [$backupName]);
             return response()->json(['message' => 'Backup realizado exitosamente']);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Error al realizar el respaldo'], 500);
